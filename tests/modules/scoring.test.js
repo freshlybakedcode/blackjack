b67e3ed362028bf0e4594a63000635c92f70a562
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2018-05-21 22:03:44 +0100

    Scoring.js tests

--- tests/modules/scoring.test.js
+++ tests/modules/scoring.test.js
@@ -2,8 +2,8 @@ import assert from 'assert';
 import {
   handContainsAce,
   getScores,
+  checkSplit,
   checkScore,
-  checkSplit
 } from '../../src/js/modules/scoring';
 
 const hands = [
@@ -22,10 +22,27 @@ const hands = [
     name: 'This hand does not',
     cards: [
       {
-        face: '6', suit: 'diamonds', value: 6, faceValue: 6, faceUp: true,
+        face: '5', suit: 'diamonds', value: 5, faceValue: 5, faceUp: true,
       },
       {
         face: '5', suit: 'spades', value: 5, faceValue: 5, faceUp: true,
+      },
+    ],
+  },
+];
+
+const bustHand = [
+  {
+    name: 'exceeding 21',
+    cards: [
+      {
+        face: 'K', suit: 'diamonds', value: 10, faceValue: 10, faceUp: true,
+      },
+      {
+        face: 'Q', suit: 'spades', value: 10, faceValue: 10, faceUp: true,
+      },
+      {
+        face: 'J', suit: 'spades', value: 10, faceValue: 10, faceUp: true,
       },
     ],
   },
@@ -41,6 +58,36 @@ describe('scoring.js tests', () => {
     });
     it('should return false when the hand does not have an ace', () => {
       assert.strictEqual(handContainsAce(hands[1]), false);
+    });
+  });
+  describe('getScores() function', () => {
+    it('should exist', () => {
+      assert.notStrictEqual(getScores(hands), undefined);
+    });
+    it('should return an array with two elements', () => {
+      assert.strictEqual(Array.isArray(getScores(hands)), true);
+      assert.strictEqual(getScores(hands).length, 2);
+    });
+    it('should return 16 and 11 in the array with the existing test data', () => {
+      assert.strictEqual(getScores(hands)[0], 16);
+      assert.strictEqual(getScores(hands)[1], 10);
+    });
+  });
+  describe('checkSplit() function', () => {
+    it('should exist', () => {
+      assert.notStrictEqual(checkSplit(hands, 0), undefined);
+    });
+    it('should be able to determine if a hand can be split (ie: two matching face values', () => {
+      assert.strictEqual(checkSplit(hands, 0), false);
+      assert.strictEqual(checkSplit(hands, 1), true);
+    });
+  });
+  describe('checkScore() function', () => {
+    it('should exist', () => {
+      assert.notStrictEqual(checkScore(hands), undefined);
+    });
+    it('should report bust when hand value exceeds 21', () => {
+      assert.strictEqual(checkScore(bustHand, 0)[1], 'bust');
     });
   });
 });
